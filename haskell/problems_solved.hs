75885c227e486397fd375ae7386dbdda6db45af4
Milkiyas <mickyduguma@gmail.com>
2022-07-21 16:35:43 +0300

    quick sort

--- haskell/problems_solved.hs
+++ haskell/problems_solved.hs
@@ -4,3 +4,10 @@
     addEveryTwo [] = []
     addEveryTwo (x:[] ) = [x]
     addEveryTwo (x: (y: ab) ) = (x+y) : addEveryTwo ab
+
+    -- quick_sort 
+    quickSort :: [Integer] -> [Integer]
+    quickSort [] = []
+    quickSort (q:qs) = quickSort ys ++ [q] ++ quickSort yz
+        where ys = [ a | a <- qs, a <=  q ]
+              yz = [ b | b<- qs , b> q]
